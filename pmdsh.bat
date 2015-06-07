d55a9154080c7a22f5c2c54a5d543c3b40f15010
Ted Mittelstaedt <tedm@mittelstaedt.us>
2015-06-06 17:26:22 -0700

    Delete pmdsh.bat

--- pmdsh.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-java -classpath c:\pmd-bin-5.3.2\lib\* net.sourceforge.pmd.PMD -dir " %1 %2 %3 %4 %5 %6 %7 " -format text -R java-basic,java-design -version 1.7 -language java
-pause
-exit
